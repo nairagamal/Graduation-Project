191af01f9697f4ee906d5af6e4d57abe8f406de7
nairagamal <nairagemi75@gmail.com>
2024-03-23 02:14:33 +0200

    .

--- ECommerce/ECommerce/ECommerce.API/obj/Debug/net6.0/ECommerce.API.AssemblyInfo.cs
+++ ECommerce/ECommerce/ECommerce.API/obj/Debug/net6.0/ECommerce.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f642e27138f1b588dd59157edb69ce0ca9946dcb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
