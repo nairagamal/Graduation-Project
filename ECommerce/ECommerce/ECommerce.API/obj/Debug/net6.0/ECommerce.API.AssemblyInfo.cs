2cc5bfd493aacc2b6d808973ba80d708086ca977
Fares Hassan <fareshaxxan@gmail.com>
2024-03-26 12:24:24 +0200

    Ui Second edit.

--- ECommerce/ECommerce/ECommerce.API/obj/Debug/net6.0/ECommerce.API.AssemblyInfo.cs
+++ ECommerce/ECommerce/ECommerce.API/obj/Debug/net6.0/ECommerce.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26522033ea1d50cc889759bc89a8ba5f4ff6c6f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ed5baa980ed92cf5ae6154b75fad9fc390cbf95")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
