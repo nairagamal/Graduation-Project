26522033ea1d50cc889759bc89a8ba5f4ff6c6f8
Fares Hassan <fareshaxxan@gmail.com>
2024-03-26 10:22:57 +0200

    Small edits.

--- ECommerce/ECommerce/ECommerce.API/obj/Release/net6.0/ECommerce.API.AssemblyInfo.cs
+++ ECommerce/ECommerce/ECommerce.API/obj/Release/net6.0/ECommerce.API.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce.API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f93eb9af7fdd3e3756a0ec56e1b61412e124170")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce.API")]
