e71ea2c573f8d33ed741186cacbd72296db3437e
nairagamal <nairagemi75@gmail.com>
2024-03-27 15:24:25 +0200

    Complete User Crud

--- ECommerce/ECommerce/ECommerce.API/obj/Debug/net6.0/ECommerce.API.AssemblyInfo.cs
+++ ECommerce/ECommerce/ECommerce.API/obj/Debug/net6.0/ECommerce.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ace99a480f34ffe6dcb2eca2ad6c9ec6b51c508")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+715502dd05bdb8fa85dd11358a58adef5fe7aa23")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ECommerceUI/src/app/user-management/user-management.component.ts
+++ ECommerceUI/src/app/user-management/user-management.component.ts
@@ -11,7 +11,19 @@ export class UserManagementComponent implements OnInit {
   users: User[] = [];
   selectedUser: User | null = null;
   editingUser: User | null = null; // Declare editingUser as potentially null
+  showAddUserForm: boolean = false;
   userFormSubmitted: boolean = false; // Flag to track form submission
+  newUser: User = { // Initialize an empty user object to bind form inputs
+    id: 0,
+    firstName: '',
+    lastName: '',
+    email: '',
+    address: '',
+    mobile: '',
+    password: '',
+    createdAt: '',
+    modifiedAt: ''
+  };
 
   constructor(private userService: UtilityService) { }
 
@@ -54,23 +66,29 @@ export class UserManagementComponent implements OnInit {
     this.userFormSubmitted = true; // Set form submission flag to true
     if (this.editingUser) {
       if (this.isValidForm()) {
+        // Update the user in the list locally
+        const index = this.users.findIndex(user => user.id === this.editingUser?.id);
+        if (index !== -1) {
+          this.users[index] = { ...this.editingUser! };
+        }
+
+        // Send request to update user on the server
         this.userService.editUser(this.editingUser.id, this.editingUser).subscribe(response => {
           if (response) {
-            // Update the user in the list
-            const index = this.users.findIndex(user => user.id === this.editingUser?.id);
-            if (index !== -1) {
-              this.users[index] = { ...this.editingUser! };
-            }
+            // If response is successful, no further action needed
             this.selectedUser = null;
-            this.editingUser = null; // Reset editingUser to null
+            this.editingUser = null;
             console.log("User updated successfully.");
           } else {
+            // If response is unsuccessful, revert the change in the local list
+            this.users[index] = { ...this.selectedUser! };
             console.error("Failed to save user changes.");
           }
         });
       }
     }
   }
+
 
   isValidForm(): boolean {
     // Check if any input field is empty or if the mobile number doesn't meet the required pattern
@@ -89,4 +107,52 @@ export class UserManagementComponent implements OnInit {
     const mobilePattern = /^(012|011|015)[0-9]{8}$/;
     return !!mobile && mobilePattern.test(mobile);
   }
+
+
+
+
+  toggleAddUserForm(): void {
+    this.showAddUserForm = !this.showAddUserForm;
+    // Reset newUser object when hiding the form
+    if (!this.showAddUserForm) {
+      this.resetNewUser();
+    }
+  }
+
+  resetNewUser(): void {
+    // Reset newUser object to clear form inputs
+    this.newUser = {
+      id: 0,
+      firstName: '',
+      lastName: '',
+      email: '',
+      address: '',
+      mobile: '',
+      password: '',
+      createdAt: '',
+      modifiedAt: ''
+    };
+  }
+
+  addUser(): void {
+    // Add new user to the list locally
+    this.users.push(this.newUser);
+
+    // Add new user to the database
+    this.userService.addUser(this.newUser).subscribe(response => {
+      if (response) {
+        // If response is successful, no further action needed
+        console.log("User added successfully.");
+      } else {
+        // If response is unsuccessful, remove the user from the list
+        this.users.pop();
+        console.error("Failed to add user.");
+      }
+
+      // Reset the newUser object and hide the Add User form
+      this.resetNewUser();
+      this.toggleAddUserForm();
+    });
+  }
+
 }
